74bcee5ca069a56ffb5ec4bca424cd6b4c5924f9
Nicholas Selby <nselby@mit.edu>
2018-09-12 22:41:10 -0400

    uploads

--- move_html.py
+++ move_html.py
@@ -2,7 +2,12 @@
 
 import os, pexpect
 from os import listdir
-
+'''
+child = pexpect.spawn('rm -rf ../www/resources')
+child.expect(pexpect.EOF)
+child = pexpect.spawn('cp -r resources ../www/resources')
+child.expect(pexpect.EOF)
+'''
 htmls = os.listdir('.')
 for fn in htmls:
 	if fn.endswith('.html'):
