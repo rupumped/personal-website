5d31ecba6a495163b76d377d6f2d2d6fca6d6491
rupumped <rupumped@gmail.com>
2018-09-12 21:51:23 -0400

    YouTube

--- make_webpage.py
+++ make_webpage.py
@@ -10,6 +10,7 @@ p_title = re.compile(r'(<title>.*)NICHOLAS S SELBY(.*</title>)',re.DOTALL)
 p_footer = re.compile(r'<div class="footer-wrap".+</div><!-- end footer-wrap -->',re.DOTALL)
 p_footCont = re.compile(r'<div id="wsite-menus">.*</div>(</body></html>)',re.DOTALL)
 p_uploads = re.compile(r'uploads/[\d/]*(?:published/|editor/|background-images/)?',re.DOTALL)
+p_youtube = re.compile(r'<div class="wsite-youtube".*?<iframe src="resources/([\w]+)\.html".*?</div>.*?</div>.*?</div>',re.DOTALL)
 
 fn = args.filename
 fn = fn[fn.index('/')+1:]
@@ -34,6 +35,12 @@ oldResources = fn.replace(' ','%20').replace('.html','') + '_files'
 content = content.replace(oldResources,'resources')
 content = re.sub(p_uploads,'resources/',content)
 content = re.sub(r'"url":"[\d/]+(/[^/]+\.[^"]+)',r'"url":"resources\1',content)
+
+# YouTube
+yt_replace = r'''<div style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
+<iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/\1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+</div>'''
+content = re.sub(p_youtube, yt_replace, content)
 
 # Link RFly
 if 'Home' in fn:
