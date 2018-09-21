6fc23cef3d8de151c1c391c62028d05894a6e0d4
rupumped <rupumped@gmail.com>
2018-09-20 22:34:00 -0400

    About me edits

--- make_webpage.py
+++ make_webpage.py
@@ -73,7 +73,6 @@ if 'Selected' in fn:
 	content = content.replace('RUPUMPED','#drones #artificial-intelligence #global-health', 1)
 	content = content.replace('RUPUMPED','#global-health #humanitarian', 1)
 if 'About' in fn:
-	content = content.replace('alt','title')
 	emDash = u'\u2014'.encode('utf-8')
 	content = content.replace('better--',"better{0}".format(emDash))
 
